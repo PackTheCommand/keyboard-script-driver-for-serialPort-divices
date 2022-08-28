4f567e1e35d17b7773be1ba1086a9e88adf994e9
PackTheCommand <92752763+PackTheCommand@users.noreply.github.com>
2022-08-28 16:20:27 +0200

    Update main.py

--- source/keyboard-script-driver-for-serialPort-divices(1.0)/main.py
+++ source/keyboard-script-driver-for-serialPort-divices(1.0)/main.py
@@ -1,6 +1,12 @@
 import threading
 import time
 import tkinter.messagebox
+
+import gridlessEngine
+import os, winshell
+from win32com.client import Dispatch
+
+
 ProgramName="Adukey"
 import keyboard
 import key
@@ -18,11 +24,51 @@ import ConfigMenue
 
 
 ####
+from Vsvpack import absoluteScriptFolderPath
+####
+adfw=absoluteScriptFolderPath()
+pa=adfw.get(__file__)
+def autostart_q():
+
+
+    startup = winshell.startup()
+    print(startup)
+
+
+    path = os.path.join(startup, "serial_key_driver.lnk")
+    target = pa+r"\serial_key_driver.exe"
+    wDir = pa
+    icon = pa+r"\resources/tray_image.ico"
+
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(path)
+    shortcut.Targetpath = target
+    shortcut.WorkingDirectory = wDir
+    shortcut.IconLocation = icon
+    shortcut.save()
+
+
+
+
+
+
 
 
 GLB_RUNTIME_TRUE=True
 setings=vsvpack("resources","Settings")
 setings.new(False)
+
+if setings.read("firstRun")=="1":
+    pass
+    #gridlessEngine.openScreen()
+
+if (setings.read("q_ask_startup")=="1"):
+    if (tkinter.messagebox.askquestion("Serial_key_driver", "do you want to start the drivcer on startup ?",icon="question") == "yes"):
+        autostart_q()
+        setings.add("q_ask_startup","0")
+    
+    
+    #restprogram
 def noFunc():
     print("NF")
     pass
@@ -91,7 +137,7 @@ def PortKomunication():
 
                 status=False
                 print("error")
-                time.sleep(3)
+            time.sleep(2)
             continue
 
         l=serial_channel.readline()
@@ -106,7 +152,7 @@ def PortKomunication():
             l=l.replace("b'","").replace("'","")
             tkinter.messagebox.Message("pressed",l)
             key.runKey(l)
-        time.sleep(0.01)
+        time.sleep(0.1)
 
 
 
